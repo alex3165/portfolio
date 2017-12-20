e0da809b6a048d2237f95b06660afb6d73484515
Alexandre Rieux <alexr.3165@gmail.com>
2017-12-20 22:08:03 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             <li className="el"><a href="https://github.com/alex3165" target="_blank" rel="noopener noreferrer">Github</a></li>
             <li className="el"><a href="https://twitter.com/alex_picprod" target="_blank" rel="noopener noreferrer">Twitter</a></li>
             <li className="el"><a href="https://www.linkedin.com/in/alexandrerieux/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
-            <li className="el"><a href="/31_01_2017_rieux-Alexandre.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
+            <li className="el"><a href="/alexandre_rieux_20122017.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
           </ul>
         </footer>
       </div>
